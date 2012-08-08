94611142ae2b8d7c7ca9d14cf612bddbd3403318
Alexander Pirsig <alexander.pirsig@rohde-schwarz.com>
2012-08-08 09:42:35 +0200

    adding money format unit tests

---
+++ tests/format_money.js
@@ -0,0 +1,42 @@
+var should = require('should');
+var toolbelt = require(__dirname+'/../index');
+
+describe('format/money',function(){
+
+    var money;
+
+    describe('money',function(){
+
+        it('should be loadable',function(){
+
+             money = toolbelt.pick('format/money');
+             money.should.be.a('function');
+        });
+
+        it('return a string', function(){
+            var formatedString = money(1000.1151, 3, ',','.');
+            formatedString.should.be.a('string');
+        });
+
+        it('should respect decimal length', function(){
+            var formatedString = money(1000.1151, 3, ',','.');
+            formatedString.should.be.equal('1.000,115');
+        });
+
+        it('should respect decimal delimiter', function(){
+            var formatedString = money(1000.111, 2, '.',',');
+            formatedString.should.be.equal('1,000.11');
+        });
+
+        it('should respect thousands separator', function(){
+            var formatedString = money(1000.111, 2, '.','-');
+            formatedString.should.be.equal('1-000.11');
+        });
+
+        it('should use default formating parameters', function(){
+            var formatedString = money(1000.111);
+            formatedString.should.be.equal('1.000,11');
+        });
+
+    });
+});

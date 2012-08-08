d56bd9e3e308cbeadb8445be3abc08a355b71303
Alexander Pirsig <apirsig@web.de>
2012-08-08 14:33:19 +0200

    fix unit tests to work with CI

--- tests/format_date.js
+++ tests/format_date.js
@@ -5,7 +5,7 @@ describe('format/date', function(){
 
     var dateFormat;
 
-    // 2012-08-07 19:39:27
+    // 2012-08-07 17:39:27 UTC
     var testTime = (new Date(1344361167946));
 
     it('should be loadable',function(){
@@ -19,6 +19,6 @@ describe('format/date', function(){
         var date = dateFormat('Y-m-d H:i:s',testTime);
 
         date.should.be.a('string');
-        date.should.be.eql('2012-08-07 19:39:27');
+        date.should.be.eql('2012-08-07 '+testTime.getHours()+':39:27');
     });
 });

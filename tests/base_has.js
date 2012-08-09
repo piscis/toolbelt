a224621b48878cffa2bdd2244097513e9afa4f39
Alexander Pirsig <apirsig@web.de>
2012-08-09 09:37:34 +0200

    adding tool to check property path

---
+++ tests/base_has.js
@@ -0,0 +1,22 @@
+var should = require('should');
+var toolbelt = require(__dirname+'/../index');
+
+describe('base/has', function(){
+
+    var has;
+    var fixture = {foo:{bar:{baz:"snafu"}}};    
+
+    it('should be loadable',function(){
+         has = toolbelt.pick('base/has');
+         has.should.be.a('function');
+    });
+
+    it('should return false on a none valid path',function(){
+
+        has(fixture, "foo.bar.bam").should.be.not.ok;
+    });
+
+    it('should return true on a valid path',function(){
+        has(fixture,"foo.bar.baz").should.be.ok;
+    });
+});

347983c4dbd29f1dcf4a1f591d850f070845409f
Alexander Pirsig <apirsig@web.de>
2012-08-22 22:02:39 +0200

    fixing natsort append problem

--- tests/sort_natsort.js
+++ tests/sort_natsort.js
@@ -3,14 +3,15 @@ var toolbelt = require(__dirname+'/../index');
 
 describe('sort/natsort', function(){
 
-    var natsort;
+    var nats;
     var ipList = ['10.5.2.2','10.5.1.6','10.5.1.1'];
     var quality = ['A1','1X','B1'];
+    var appended = {};
 
     it('should be loadable',function(){
 
-         natsort = toolbelt.pick('sort/natsort');
-         natsort.should.be.a('function');
+         nats = toolbelt.pick('sort/natsort');
+         nats.should.be.a('function');
     });
 
     it('should sort an list of IP addresses',function(){
@@ -21,7 +22,7 @@ describe('sort/natsort', function(){
             '10.5.2.2'
         ];
 
-        var res = ipList.sort(natsort);
+        var res = ipList.sort(nats);
 
         res.should.be.an.instanceOf(Array);
         res.should.be.eql(resFinal);
@@ -35,9 +36,26 @@ describe('sort/natsort', function(){
             'B1'
         ];
 
-        var res = quality.sort(natsort);
+        var res = quality.sort(nats);
 
         res.should.be.an.instanceOf(Array);
         res.should.be.eql(resFinal);
     });
+
+    it('should be okay when appended to an object',function(){
+
+        var resFinal = [
+            '1X',
+            'A1',
+            'B1'
+        ];
+
+        appended.nats = nats;
+
+        var res = quality.sort(appended.nats);
+
+        res.should.be.an.instanceOf(Array);
+        res.should.be.eql(resFinal);
+    });
+
 });
